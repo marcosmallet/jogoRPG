ba33fa88bcf056ce5a7dcd2e36e98ef7d986e5fd
Marcos Mallet <marcosmallet.nasajon@gmail.com>
2015-10-28 13:16:50 -0200

    Funções de criação dos personagens: Humano e Orc

--- index.php
+++ index.php
@@ -11,3 +11,32 @@ class TipoPersonagem {
     public $agilidade;
     public $arma;
 }
+
+$humano;
+$orc;
+$contRodada;
+$vez;
+
+function CriarHumano(){
+	global $humano;
+	$humano = new TipoPersonagem();
+	$humano->vida = 12;
+	$humano->forca = 1;	
+	$humano->agilidade = 2;	
+	$humano->arma = new TipoArma();
+	$humano->arma->ataque = 2;
+	$humano->arma->defesa = 1;
+	$humano->arma->numeroFacesDado = 6;		
+}
+
+function CriarOrc(){
+	global $orc;
+	$orc = new TipoPersonagem();
+	$orc->vida = 20;
+	$orc->forca = 2;	
+	$orc->agilidade = 0;	
+	$orc->arma = new TipoArma();
+	$orc->arma->ataque = 1;
+	$orc->arma->defesa = 0;
+	$orc->arma->numeroFacesDado = 8;		
+}
