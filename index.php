9195440ec97908f9030e1b2e9599e49708a26a8d
Marcos Mallet <marcosmallet.nasajon@gmail.com>
2015-10-28 13:14:12 -0200

    Criação das Classes dos Personagens

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+class TipoArma {
+    public $ataque;
+    public $defesa;
+    public $numeroFacesDado;
+}
+
+class TipoPersonagem {
+    public $vida;
+    public $forca;
+    public $agilidade;
+    public $arma;
+}
